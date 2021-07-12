78e8703b7ba668220dfd55371b36b27af9a95e99
Brad Treloar <brad.treloar@pm.me>
2021-07-12 12:50:20 +0930

    fix editor css

--- web/app/themes/sage/app/setup.php
+++ web/app/themes/sage/app/setup.php
@@ -39,8 +39,6 @@ add_action(
             null,
             true
         );
-        wp_enqueue_style('sage/main.css', asset_path('styles/main.css'), false, null);
-        wp_enqueue_script('sage/main.js', asset_path('scripts/main.js'), ['jquery'], null, true);
     },
     100
 );
