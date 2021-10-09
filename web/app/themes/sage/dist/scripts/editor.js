b04e1e052bb351a334888d05b41041596bb4606a
Brad Treloar <brad.treloar@pm.me>
2021-10-09 14:41:12 +1030

    update theme

--- web/app/themes/sage/dist/scripts/editor.js
+++ web/app/themes/sage/dist/scripts/editor.js
@@ -104,7 +104,7 @@ wp.domReady(function () {
 /***/ 16:
 /***/ (function(module, exports) {
 
-// removed by extract-text-webpack-plugin
+throw new Error("Module build failed: ModuleBuildError: Module build failed: \n  @import \"layouts/header\";\n ^\n      File to import not found or unreadable: layouts/header.\n      in /home/brad/Sites/wordpress-basic-starter/wordpress/web/app/themes/sage/resources/assets/styles/editor.scss (line 10, column 3)\n    at /home/brad/Sites/wordpress-basic-starter/wordpress/web/app/themes/sage/node_modules/webpack/lib/NormalModule.js:195:19\n    at /home/brad/Sites/wordpress-basic-starter/wordpress/web/app/themes/sage/node_modules/loader-runner/lib/LoaderRunner.js:367:11\n    at /home/brad/Sites/wordpress-basic-starter/wordpress/web/app/themes/sage/node_modules/loader-runner/lib/LoaderRunner.js:233:18\n    at context.callback (/home/brad/Sites/wordpress-basic-starter/wordpress/web/app/themes/sage/node_modules/loader-runner/lib/LoaderRunner.js:111:13)\n    at Object.callback (/home/brad/Sites/wordpress-basic-starter/wordpress/web/app/themes/sage/node_modules/sass-loader/lib/loader.js:55:13)\n    at Object.done [as callback] (/home/brad/Sites/wordpress-basic-starter/wordpress/web/app/themes/sage/node_modules/neo-async/async.js:8069:18)\n    at options.error (/home/brad/Sites/wordpress-basic-starter/wordpress/web/app/themes/sage/node_modules/node-sass/lib/index.js:293:32)");
 
 /***/ })
 
